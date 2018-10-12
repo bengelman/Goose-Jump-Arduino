f57dc07cd346b167a239bf70ef0161ecd3c39509
bengelman <brendan.engelman@gmail.com>
2018-10-11 22:31:36 -0400

    Implemented class for storing theme information

--- HeadsUpV1/HeadsUpV1.ino
+++ HeadsUpV1/HeadsUpV1.ino
@@ -1,9 +1,34 @@
+
+
+class Theme{
+  public:
+   
+    Theme(String name, String promptSet[], int length){
+      title = name;
+      size = length;
+      for (int i = 0; i < length; i++){
+        prompts[i] = promptSet[i];
+      }
+    }
+    int size;
+    String title; 
+    String getRandomPrompt();
+  private:
+    
+    String prompts[100];
+};
+String promptWaterloo[] = {"Mr Goose", ""};
+Theme* themes[] = {new Theme("Waterloo", promptWaterloo, 2)};
+String Theme::getRandomPrompt(){
+  return prompts[rand() % size];
+}
+
 void setup() {
   // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
