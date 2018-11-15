ccd8ecbd8f29d22da84d72a587c7987adc267d1e
bengelman <brendan.engelman@gmail.com>
2018-11-15 11:19:00 -0500

    Began porting project to c++ for Arduino Everything that can be ported given the hardware in our possession has now been implemented, except the prompt list

--- HeadsUpV1/HeadsUpV1.ino
+++ HeadsUpV1/HeadsUpV1.ino
@@ -12,23 +12,78 @@ class Theme{
     }
     int size;
     String title; 
-    String getRandomPrompt();
+    int getRandomPrompt();
+    String prompts[100];
   private:
     
-    String prompts[100];
+    
 };
-String promptWaterloo[] = {"Mr Goose", ""};
-Theme* themes[] = {new Theme("Waterloo", promptWaterloo, 2)};
-String Theme::getRandomPrompt(){
-  return prompts[rand() % size];
+enum GameState{
+  SELECT, PLAY, END
+};
+GameState state = SELECT;
+int score = 0;
+float timer = 0;
+int theme = 0;
+int prompt = 0;
+String promptWaterloo[] = {"Mr Goose", "SYDE"};
+String promptMovies[] = {"Whiplash", "Grease"};
+int themeNum = 1;
+Theme themes[] = {
+  Theme("Waterloo Memes", promptWaterloo, 2),
+  Theme("Movies", promptMovies, 2);
+};
+int Theme::getRandomPrompt(){
+  rand() % Theme::size;
 }
 
 void setup() {
   // put your setup code here, to run once:
   
 }
-
+float lastMillis = 0;
 void loop() {
+  if (timer > 0){
+    timer -= (millis() - lastMillis);
+  }
+  lastMillis = millis();
+  if (state == SELECT){
+    displayText(themes[theme].title);
+    if (timer <= 0){
+      timer = 1000;
+      theme++;
+      theme %= themeNum;
+    }
+    if (gyro()){
+      timer = 60000;
+      state = PLAY;
+    }
+  }
+  if (state == PLAY){
+    displayText(themes[theme].prompts[prompt]);
+    if (gyro()){
+      score++;
+      prompt = themes[theme].getRandomPrompt();
+    }
+    if (timer <= 0){
+      state = END;
+      timer = 5000;
+    }
+    if (state == END){
+      displayText("Score: " + score);
+      if (timer <= 0){
+        state = SELECT;
+        timer = 1000;
+      }
+    }
+  }
   // put your main code here, to run repeatedly:
   
+}
+void displayText(String toDisplay){
+  
+}
+
+bool gyro(){
+  return false;
 }
